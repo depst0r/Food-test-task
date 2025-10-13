759d26801a698c1fcbfd677e20e68101198fa538
depstor <depstor90@gmail.com>
2025-10-13 20:37:19 +0300

    timer

--- dist/js/script.js
+++ dist/js/script.js
@@ -3,6 +3,7 @@
   !*** ./src/js/main.js ***!
   \************************/
 window.addEventListener('DOMContentLoaded', () => {
+  //Tabs
   const tabs = document.querySelectorAll('.tabheader__item');
   const tabsContent = document.querySelectorAll('.tabcontent');
   const tabsParent = document.querySelector('.tabheader__items');
@@ -33,6 +34,51 @@ window.addEventListener('DOMContentLoaded', () => {
   });
   hidetabContent();
   showTabContent();
+
+  // Timer
+
+  const deadline = '2025-10-15';
+  function getTimeRemaining(endtime) {
+    const t = Date.parse(endtime) - Date.parse(new Date());
+    const days = Math.floor(t / (1000 * 60 * 60 * 24));
+    const hours = Math.floor(t / 1000 * 60 * 60 % 24);
+    const minutes = Math.floor(t / 1000 / 60 % 60);
+    const seconds = Math.floor(t / 1000 % 60);
+    return {
+      'total': t,
+      'days': days,
+      'hours': hours,
+      'minutes': minutes,
+      'seconds': seconds
+    };
+  }
+  function getZero(num) {
+    if (num >= 0 && num < 10) {
+      return `0${num}`;
+    } else {
+      return num;
+    }
+  }
+  function setClock(selector, endTime) {
+    const timer = document.querySelector(selector);
+    const days = timer.querySelector('#days');
+    const hours = timer.querySelector('#hours');
+    const minutes = timer.querySelector('#minutes');
+    const seconds = timer.querySelector('#seconds');
+    const timeInterval = setInterval(updateClock, 1000);
+    updateClock();
+    function updateClock() {
+      const t = getTimeRemaining(endTime);
+      days.innerHTML = getZero(t.days);
+      hours.innerHTML = getZero(t.hours);
+      minutes.innerHTML = getZero(t.minutes);
+      seconds.innerHTML = getZero(t.seconds);
+      if (t.total <= 0) {
+        clearInterval(timeInterval);
+      }
+    }
+  }
+  setClock('.timer', deadline);
 });
 /******/ })()
 ;
